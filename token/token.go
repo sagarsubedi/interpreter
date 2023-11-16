1035ef555d66bf6ed4a067870923d746144870cd
Sagar Subedi <sagarsubediofficial@gmail.com>
2023-11-16 00:20:21 -0600

    init: tokenize simple statements and function

---
+++ token/token.go
@@ -0,0 +1,48 @@
+package token
+
+// * int or byte might be performant
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers and literals
+	IDENT  = "IDENT"
+	INT    = "INT"
+	STRING = "STRING"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
